4d75d3bb7832850c4c059d224b45e1da458ef5f9
wangyuec15 <756392174@qq.com>
2022-01-07 18:36:48 -0500

    map validation

--- ppmtool/src/main/java/wangyuec15/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/wangyuec15/ppmtool/web/ProjectController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import wangyuec15.ppmtool.domain.Project;
+import wangyuec15.ppmtool.services.MapValidationErrorService;
 import wangyuec15.ppmtool.services.ProjectService;
 
 @RestController
@@ -27,18 +28,16 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+	
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-		if(result.hasErrors()) {
-			Map<String, String> errorMap = new HashMap<>();
-			
-			for(FieldError error: result.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-			}
-			
-			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-		}
 		
+		ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+		if(errorMap != null) {
+			return errorMap;
+		}
 		
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
