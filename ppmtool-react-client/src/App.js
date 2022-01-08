6a9f2d4e770ddadff07248f163e299a44a547592
wangyuec15 <756392174@qq.com>
2022-01-08 13:22:29 -0500

    project and header components

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Dashboard />
     </div>
   );
