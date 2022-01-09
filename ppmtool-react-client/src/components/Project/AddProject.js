639dbf0efef0f621f922a5d0e9824651b4a4ce7d
wangyuec15 <756392174@qq.com>
2022-01-08 23:24:37 -0500

    get validation errors from redux

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -12,10 +12,17 @@ class AddProject extends Component {
       description: "",
       start_date: "",
       end_date: "",
+      errors: {}
     };
 
     this.onChange=this.onChange.bind(this);
     this.onSubmit=this.onSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.errors) {
+      this.setState({errors:nextProps.errors});
+    }
   }
 
   onChange(e) {
@@ -35,8 +42,12 @@ class AddProject extends Component {
   }
 
   render() {
+    const {errors} = this.state
+
+    
     return (
       <div>
+        
         <div className="project">
           <div className="container">
             <div className="row">
@@ -53,6 +64,7 @@ class AddProject extends Component {
                       value={this.state.projectName}
                       onChange={this.onChange}
                     />
+                    <p>{errors.projectName}</p>
                   </div>
                   <div className="form-group">
                     <input 
@@ -63,6 +75,7 @@ class AddProject extends Component {
                       value={this.state.projectIdentifier}
                       onChange={this.onChange}
                     />
+                    <p>{errors.projectIdentifier}</p>
                   </div>
                   
                   <div className="form-group">
@@ -73,6 +86,7 @@ class AddProject extends Component {
                       value={this.state.description}
                       onChange={this.onChange}
                     />
+                    <p>{errors.description}</p>
                   </div>
                   <h6>Start Date</h6>
                   <div className="form-group">
@@ -107,9 +121,14 @@ class AddProject extends Component {
 }
 
 AddProject.propTypes = {
-  createProject: PropTypes.func.isRequired
+  createProject: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
+const mapStateToProps = state =>({
+  errors:state.errors
+});
+
 export default connect(
-  null, {createProject}
+  mapStateToProps, {createProject}
   ) (AddProject);
