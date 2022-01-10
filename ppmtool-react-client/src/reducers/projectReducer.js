205413feef27790d87261bc33a2082b2b662e8f8
wangyuec15 <756392174@qq.com>
2022-01-09 22:28:20 -0500

    delete project

--- ppmtool-react-client/src/reducers/projectReducer.js
+++ ppmtool-react-client/src/reducers/projectReducer.js
@@ -1,5 +1,4 @@
-import { GET_PROJECTS } from "../actions/types"
-import { GET_PROJECT } from "../actions/types"
+import { GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "../actions/types"
 
 const initialState = {
     projects: [],
@@ -18,6 +17,11 @@ export default function(state = initialState, action) {
             return {
                 ...state,
                 project: action.payload
+            }
+        case DELETE_PROJECT:
+            return {
+                ...state,
+                projects: state.projects.filter(project => project.projectIdentifier != action.payload)
             }
         default:
             return state
