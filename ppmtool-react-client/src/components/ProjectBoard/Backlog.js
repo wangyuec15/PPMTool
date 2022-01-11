13b1041a5f6d7d05e344e895386a0f2815a66137
wangyuec15 <756392174@qq.com>
2022-01-11 14:30:19 -0500

    organize project tasks by status and priority

--- ppmtool-react-client/src/components/ProjectBoard/Backlog.js
+++ ppmtool-react-client/src/components/ProjectBoard/Backlog.js
@@ -7,43 +7,53 @@ class Backlog extends Component {
     const tasks = project_tasks_prop.map(project_task=>(
       <ProjectTask key={project_task.id} project_task = {project_task} />
     ))
+
+    let todoItems = [];
+    let inProgressItems = [];
+    let doneItems = [];
+
+    for(let i = 0; i < tasks.length; i++) {
+      console.log(tasks[i]);
+      if(tasks[i].props.project_task.status === "TO_DO") {
+        todoItems.push(tasks[i]);
+      }
+      if(tasks[i].props.project_task.status === "IN_PROGRESS") {
+        inProgressItems.push(tasks[i]);
+      }
+      if(tasks[i].props.project_task.status === "DONE") {
+        doneItems.push(tasks[i]);
+      }
+    }
+
     return (
       <div className="container">
-      <div className="row">
+        <div className="row">
           <div className="col-md-4">
               <div className="card text-center mb-2">
                   <div className="card-header bg-secondary text-white">
                       <h3>TO DO</h3>
                   </div>
               </div>
-              {tasks}
+              {todoItems}
           </div>
           <div className="col-md-4">
-              <div className="card text-center mb-2">
-                  <div className="card-header bg-primary text-white">
-                      <h3>In Progress</h3>
-                  </div>
+            <div className="card text-center mb-2">
+              <div className="card-header bg-primary text-white">
+                <h3>In Progress</h3>
               </div>
-              {
-              //   <!-- SAMPLE PROJECT TASK STARTS HERE -->
-
-              // <!-- SAMPLE PROJECT TASK ENDS HERE -->
-            }
+            </div>
+            {inProgressItems}
           </div>
           <div className="col-md-4">
-              <div className="card text-center mb-2">
-                  <div className="card-header bg-success text-white">
-                      <h3>Done</h3>
-                  </div>
+            <div className="card text-center mb-2">
+              <div className="card-header bg-success text-white">
+                <h3>Done</h3>
               </div>
-              {
-              //   <!-- SAMPLE PROJECT TASK STARTS HERE -->
-
-              // <!-- SAMPLE PROJECT TASK ENDS HERE -->
-            }
+            </div>
+            {doneItems}
           </div>
+        </div>
       </div>
-  </div>
     )
   }
 }
