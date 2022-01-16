f3931fee144c4e983d431aced759e3cb5a21b678
wangyuec15 <756392174@qq.com>
2022-01-15 22:00:08 -0500

    security action and reducer

--- ppmtool-react-client/src/actions/SecurityActions.js
+++ ppmtool-react-client/src/actions/SecurityActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setJWTToken from "../securityUtils/setJWTToken";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import jwt_decode from "jwt-decode"
 
 export const createNewUser = (newUser, history) => async dispatch => {
   try {
@@ -15,4 +17,31 @@ export const createNewUser = (newUser, history) => async dispatch => {
       payload: error.response.data
     })
   }
+}
+
+export const login = LoginRequest => async dispatch => {
+  try {
+    // post => login request
+    const res = await axios.post("/api/users/login", LoginRequest);
+    // extract token from res.data
+    const {token} = res.data;
+    // store token in the local storage
+    localStorage.setItem("jwtToken", token)
+    // set our token in header ***
+    setJWTToken(token)
+    // decode token on React
+    const decoded = jwt_decode(token);
+    // dispatch to our securityReducer
+
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: decoded
+    })
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data
+    })
+  }
+  
 }
