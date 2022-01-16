bf00aa0765384b9e046661b4481ad84c22be3534
wangyuec15 <756392174@qq.com>
2022-01-16 12:38:16 -0500

    dynamic header based on security state

--- ppmtool-react-client/src/components/Layout/Header.js
+++ ppmtool-react-client/src/components/Layout/Header.js
@@ -1,27 +1,23 @@
 import React, { Component } from 'react'
 import {Link} from "react-router-dom"
+import PropTypes from "prop-types"
+import {connect} from "react-redux"
+import { logout } from '../../actions/SecurityActions'
+
 
 class Header extends Component {
+
+  logout() {
+    this.props.logout();
+    window.location.href = "/"
+  }
+
   render() {
-    return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
-        <div className="container">
-          <Link to={"/"} className="navbar-brand">
-              Personal Project Management Tool
-          </Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
-              <span className="navbar-toggler-icon" />
-          </button>
+    const {validToken, user} = this.props.security
 
-          <div className="collapse navbar-collapse" id="mobile-nav">
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link to={"/dashboard"} className="nav-link">
-                    Dashboard
-                  </Link>
-                </li>
-            </ul>
-
+    const userIsNotAuthenticated = (
+      <div className="collapse navbar-collapse" id="mobile-nav">
+            
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
                 <Link to={"/register"} className="nav-link ">
@@ -35,10 +31,76 @@ class Header extends Component {
               </li>
             </ul>
           </div>
+    )
+
+    const userIsAuthenticated = (
+      <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link to={"/dashboard"} className="nav-link">
+                    Dashboard
+                  </Link>
+                </li>
+            </ul>
+
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link to={"/dashboard"} className="nav-link ">
+                <i className='fas fa-user-circle mr-1' />
+                  
+                { user.fullname}
+                  
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/logout"} className="nav-link"
+                onClick = {this.logout.bind(this)}
+                >
+                  Logout
+                  
+                </Link>
+              </li>
+            </ul>
+          </div>
+    )
+
+      let headerLinks;
+
+      if(validToken && user) {
+        headerLinks = userIsAuthenticated
+      } else {
+        headerLinks = userIsNotAuthenticated
+      }
+
+
+
+
+
+
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+        <div className="container">
+          <Link to={"/"} className="navbar-brand">
+              Personal Project Management Tool
+          </Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+              <span className="navbar-toggler-icon" />
+          </button>
+          {headerLinks}
+          
         </div>
       </nav>
     )
   }
 }
 
-export default Header;
+Header.propTypes = {
+  logout: PropTypes.func.isRequired,
+  security: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  security: state.security
+})
+
+export default connect(mapStateToProps, {logout})(Header);
